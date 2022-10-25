9dc2fa70b530efed25491b5d9c7023e21d8a36fb
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-25 16:24:11 -0400

    hrefs added

--- src/components/external-links.jsx
+++ src/components/external-links.jsx
@@ -2,14 +2,14 @@ export default function External() {
     return (<div id="external-links">
         <h1>External Links</h1>
         <ul>
-            <li><a href="#">Frontend GitHub Repo</a></li>
-            <li><a href="#">Backend GitHub Repo</a></li>
-            <li><a href="#">Planning Documents GitHub Repo</a> </li>
+            <li><a href="https://github.com/pikeminnow/phase-3-rogue-frontend">Frontend GitHub Repo</a></li>
+            <li><a href="https://github.com/pikeminnow/phase-3-rogue-backend">Backend GitHub Repo</a></li>
+            <li><a href="https://github.com/pikeminnow/phase-3-rogue-planning-documents">Planning Documents GitHub Repo</a> </li>
         </ul>
         <h2>Credits</h2>
         <ul>
-            <li><a href="#">Seo Choi</a></li>
-            <li><a href="#">Ashton MacKenzie</a></li>
+            <li><a href="https://www.linkedin.com/in/seo-choi-dev/">Seo Choi</a></li>
+            <li><a href="https://ashtonmackenzie.com/">Ashton MacKenzie</a></li>
         </ul>
     </div>);
 }
