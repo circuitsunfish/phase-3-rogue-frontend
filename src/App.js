c00d9e98f308c6fd1ed72594cf313bdde1c8e466
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-25 16:07:40 -0400

    getting ready for components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 
 function App() {
 
-  const [game, setGame] = useState({session_name: ""});
+  const [game, setGame] = useState({ session_name: "" });
 
   useEffect(() => {
     fetch(`http://localhost:9292/start_game`)
-    .then((r) => r.json())
-      .then((game) =>  {
+      .then((r) => r.json())
+      .then((game) => {
         setGame(game)
-        console.log(game)}
-        );
+        console.log(game)
+      }
+      );
   }, []);
-  
+
 
   // if (!game) return <h2>Loading game data...</h2>;
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {game["session_name"]}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's PLAY SOME ROGUE BABY!
-        </a>
-      </header>
+
     </div>
   );
 }
