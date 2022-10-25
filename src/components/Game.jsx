1653b27b09ed8c8eb0659d0255540d9192576635
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-25 16:43:59 -0400

    how to play draft

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,10 @@
-export default function Game(){
+import HowToPlay from "./How2Play"
+
+export default function Game() {
     return (
         <div id="Game_Container">
             This is our container for game components
+            <HowToPlay />
         </div>
     )
 }
---
+++ src/components/How2Play.jsx
@@ -0,0 +1,13 @@
+export default function HowToPlay() {
+    return (<div id="howToPlay">
+        <h2>How To Play</h2>
+        <ol>
+            <li>Check the Options first as they will interrupt your existing game.</li>
+            <li>Start a new game or load an existing save.</li>
+            <li>Use [CONTROLS] to move and attack.</li>
+            <li>Defeat any monsters and make it safely to your bed.</li>
+            <li>Save your game if you want to come back later.</li>
+            <li>Are you your own worst fear? Defeat your past selves!</li>
+        </ol>
+    </div>);
+}
