7c5bda0c26286ec86879e54b42794af341ea8217
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-25 16:51:12 -0400

    gamearea placeholder

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,11 @@
+import GameareaPlaceholder from "./GameareaPlaceholder"
 import HowToPlay from "./How2Play"
 
 export default function Game() {
     return (
         <div id="Game_Container">
             This is our container for game components
+            <GameareaPlaceholder />
             <HowToPlay />
         </div>
     )
