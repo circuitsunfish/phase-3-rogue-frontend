e47984d71418911498cbc634104835a570cfc69d
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-26 18:51:08 -0400

    updated howtoplay with correct controls

--- src/components/How2Play.jsx
+++ src/components/How2Play.jsx
@@ -4,7 +4,7 @@ export default function HowToPlay() {
         <ol>
             <li>Check the Options first as they will interrupt your existing game.</li>
             <li>Start a new game or load an existing save.</li>
-            <li>Use [CONTROLS] to move and attack.</li>
+            <li>Use [WASD / arrow keys] to move and attack.</li>
             <li>Defeat any monsters and make it safely to your bed.</li>
             <li>Save your game if you want to come back later.</li>
             <li>Are you your own worst fear? Defeat your past selves!</li>
