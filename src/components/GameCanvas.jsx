0790a06408251a1f89e3a44b3ef19be7d647ed97
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-26 18:32:00 -0400

    ninja out of jail, it was a race condition. always use arrow syntax in setter function

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -1,16 +1,7 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react'
 
-const GameCanvas = () => {
-    const [player_x, setPlayer_x] = useState(85);
-    const [player_y, setPlayer_y] = useState(75);
-
-    console.log("rendering game canvas");
-
-    const handleKeyPress = useCallback((event) => {
-        console.log(`Key pressed: ${event.key}`);
-        handleKey(event);
-        console.log({ player_x, player_y });
-    }, []);
+const GameCanvas = ({ player_x, player_y }) => {
+    console.log({ player_x })
     const canvasRef = useRef(null)
 
     const draw = ctx => {
@@ -44,50 +35,6 @@ const GameCanvas = () => {
         ctx.fillText(`Canvas Height: ${ctx.canvas.height}`, 10, 10)
         ctx.fillText(`Canvas Width: ${ctx.canvas.width}`, 20, 20)
     }
-
-    // controls start
-
-    const handleKey = (e) => {
-        switch (e.keyCode) {
-            case 38:
-            case 87:
-                // player_y = player_y - 20;
-                setPlayer_y(player_y - 20);
-                console.log("top")
-                break;
-            case 68:
-            case 39:
-                // player_x = player_x + 20;
-                setPlayer_x(player_x + 20);
-                console.log("right")
-                break;
-            case 83:
-            case 40:
-                // player_y = player_y + 20;
-                setPlayer_y(player_y + 20);
-                console.log("bottom")
-                break;
-            case 65:
-            case 37:
-                // player_x = player_x - 20;
-                setPlayer_x(player_x - 20);
-                console.log("left")
-                break;
-        }
-    };
-
-    useEffect(() => {
-        // attach the event listener
-        document.addEventListener('keydown', handleKeyPress);
-
-        // remove the event listener
-        return () => {
-            document.removeEventListener('keydown', handleKeyPress);
-        };
-    }, [handleKeyPress]);
-
-    //controls end
-
 
     useEffect(() => {
 
--- src/components/GameLogic.jsx
+++ src/components/GameLogic.jsx
@@ -4,6 +4,63 @@ import GameCanvas from './GameCanvas'
 
 export default function GameLogic() {
 
+    const [player_x, setPlayer_x] = useState(85);
+    const [player_y, setPlayer_y] = useState(75);
+
+    console.log("rendering game canvas");
+
+    const handleKeyPress = useCallback((event) => {
+        console.log(`Key pressed: ${event.key}`);
+        handleKey(event);
+        console.log({ player_x, player_y });
+    }, []);
+
+
+    // controls start
+
+    const handleKey = (e) => {
+        switch (e.keyCode) {
+            case 38:
+            case 87:
+                // player_y = player_y - 20;
+                setPlayer_y((currentY) => currentY - 20);
+                console.log("top")
+                break;
+            case 68:
+            case 39:
+                // player_x = player_x + 20;
+                setPlayer_x((currentX) => currentX + 20);
+                console.log("right")
+                break;
+            case 83:
+            case 40:
+                // player_y = player_y + 20;
+                setPlayer_y((currentY) => currentY + 20);
+                console.log("bottom")
+                break;
+            case 65:
+            case 37:
+                // player_x = player_x - 20;
+                setPlayer_x((currentX) => currentX - 20);
+                console.log("left")
+                break;
+        }
+    };
+
+    useEffect(() => {
+        // attach the event listener
+        document.addEventListener('keydown', handleKeyPress);
+
+        // remove the event listener
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleKeyPress]);
+
+    //controls end
+
+
+    return <GameCanvas player_x={player_x} player_y={player_y} />
 
 
 }
